ea4a466c5ee6ae869c59a2676420f52b52535877
Andreas Stassivik <andreas@stassi.net>
2021-10-22 14:10:02 -0700

    superfluous class removal
    Signed-off-by: Andreas Stassivik <andreas@stassi.net>

--- src/legacy/ProtocolResolver.js
+++
@@ -1,8 +0,0 @@
-import valveProtocol from './ValveProtocol.js'
-
-export default class ProtocolResolver {
-  create(protocolId) {
-    if (protocolId === 'valve') return new valveProtocol()
-    else throw new Error('unsupported protocol')
-  }
-}
