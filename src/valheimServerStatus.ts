3515d6c9a2c1455349761f2761296a2b7d6c65a8
Andreas Stassivik <andreas@stassi.net>
2021-10-22 14:09:27 -0700

    query implementation
    Signed-off-by: Andreas Stassivik <andreas@stassi.net>

--- src/valheimServerStatus.ts
+++ src/valheimServerStatus.ts
@@ -1,29 +1,12 @@
-import type {
-  QueryResult,
-  QueryOptions as LegacyQueryOptions,
-  Type as LegacyQueryType,
-} from 'gamedig'
-// @ts-ignore
-import QueryRunner from './legacy/QueryRunner.js'
-
-type PromiseQueryResult = Promise<QueryResult>
-
-function serverStatus(
-  userOptions: Omit<LegacyQueryOptions, 'type'> & {
-    type: LegacyQueryType | 'valheim'
-  }
-): PromiseQueryResult {
-  return new QueryRunner(undefined).run(userOptions)
-}
+import type { QueryResult } from 'gamedig'
+import query from './query'
 
 export default function valheimServerStatus(
   host: string,
   port: number = 10011
-): PromiseQueryResult {
-  return serverStatus({
+): Promise<QueryResult> {
+  return query({
     host,
     port,
-    givenPortOnly: true,
-    type: 'valheim',
   })
 }
