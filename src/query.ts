57fe04e1d75e14e9a6b659591219599de6abbb3f
Andreas Stassivik <andreas@stassi.net>
2021-10-22 19:56:41 -0700

    superfluous parameter removal
    Signed-off-by: Andreas Stassivik <andreas@stassi.net>

--- src/query.ts
+++ src/query.ts
@@ -1,17 +1,16 @@
-import type { QueryOptions as LegacyQueryOptions, QueryResult } from 'gamedig'
+import type { QueryOptions, QueryResult } from 'gamedig'
 import UDPSocket from './UDPSocket'
 // @ts-ignore
 import ValveProtocol from './legacy/ValveProtocol.js'
 
 export default function query({
   attemptTimeout = 10000,
-  maxAttempts = 1,
   port = 2456,
   socketTimeout = 2000,
   ...props
 }: Omit<
-  LegacyQueryOptions,
-  'givenPortOnly' | 'requestRules' | 'type'
+  QueryOptions,
+  'givenPortOnly' | 'maxAttempts' | 'requestRules' | 'type'
 >): Promise<QueryResult> {
   const core = new ValveProtocol()
 
@@ -19,7 +18,6 @@ export default function query({
 
   core.options = {
     attemptTimeout,
-    maxAttempts,
     port,
     socketTimeout,
     ...props,
